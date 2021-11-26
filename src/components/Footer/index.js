ba687be8c19c7142e5ed803c6b09e35b29e63975
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-11-26 10:29:18 -0300

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import './Footer.sass';
-import { AiFillInstagram, AiFillGithub, AiOutlineWhatsApp } from 'react-icons/ai'
+
 
 const Footer = () => {
     return(
+       
         <footer>
-            <div className = 'textfooter'>  
+            <div>  
                 <p>info@meusite.com</p>
                 <p>Tel: XXX-XXXX-XXXX</p>
             </div>
-            <div className = "iconfooter">
-                <AiFillInstagram />
-                <AiFillGithub />
-                <AiOutlineWhatsApp />
-            </div>
             <p>Feito por João Henrique. 2021</p>
         </footer>
+       
     );
 }
 
