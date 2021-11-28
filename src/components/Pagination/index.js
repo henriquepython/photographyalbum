9e119106e3d8ba821336f77664a67ba46c65dda5
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-11-28 13:54:05 -0300

    update axios

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import api from '../../services/api'
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api.js'
 import './Pagination.sass';
 
 
-
 const Pagination = () => {
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        api
+        .get('/photos/2014422', {
+            headers: {
+              Authorization: '563492ad6f91700001000001d717ba1d15da4f06bcc63b3eb177e0ba'
+            }
+           })
+        .then((response) => setUser(response.data))
+        .catch((err) => {
+            console.error("ops! ocorreu um erro" + err);
+        });
+    }, []);
     return(
-        <></>   
+        <>
+            {<div> <img src='{JSON.stringify(user.original)}' /></div>}
+        </>   
     );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
-import { createClient } from 'pexels';
+import axios from "axios";
 
-const client = createClient('563492ad6f91700001000001d717ba1d15da4f06bcc63b3eb177e0ba');
+const api = axios.create({
+  baseURL: "https://api.pexels.com/v1",
+});
 
-client.photos.curated({ per_page: 15 }).then(photos => {...});
-
-console.log(photos);
-
+export default api;
