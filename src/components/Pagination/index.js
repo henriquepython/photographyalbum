ca02fe323c832d84fc9944194eabbbc2fc34b9b1
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-11-30 16:53:05 -0300

    update buttons album

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import ButtonPagination from '../ButtonPagination';
 import api from '../../services/api.js'
 import './Pagination.sass';
 
@@ -9,7 +9,7 @@ const Pagination = () => {
 
     useEffect(() => {
         api
-        .get('/curated?page=2&per_page=40', {
+        .get('/curated?page=2&per_page=30', {
             headers: {
               Authorization: '563492ad6f91700001000001d717ba1d15da4f06bcc63b3eb177e0ba'
             }
@@ -22,19 +22,21 @@ const Pagination = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return(
-        
-            <div className ="images">
+            <>
+            <ButtonPagination className="buttons-top"/>
+            <div className = "images">
                 {user?.map((item) => (
                     <img className = "item"
                         key = {item.id}
                         id={item.id}
                         title={item.photographer}
-                        src={item.src['small']}
+                        src={item.src['medium']}
                         liker={item.liked}
                     />
                 ))}
             </div>
-           
+            <ButtonPagination className="buttons-down" />,
+            </>
     );
 }
 
