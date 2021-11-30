430ba35b79e6fd4c605518193d3ac72c623839c3
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-11-30 18:24:19 -0300

    update useeffect

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import ButtonPagination from '../ButtonPagination';
 import api from '../../services/api.js'
 import './Pagination.sass';
 
 
 const Pagination = () => {
     const [user, setUser] = useState();
+    const [page, setPage] = useState(1);
 
+    function next () {
+        let pages = page + 1
+        return setPage(pages)
+    }
+
+    function prev () {
+        if (page > 1){
+            let pages = page - 1
+            return setPage(pages)
+        }
+        return page
+    }
+    
     useEffect(() => {
         api
-        .get('/curated?page=2&per_page=30', {
+        .get(`/curated?page=${page}&per_page=30`, {
             headers: {
               Authorization: '563492ad6f91700001000001d717ba1d15da4f06bcc63b3eb177e0ba'
             }
@@ -20,10 +33,15 @@ const Pagination = () => {
         console.log(user)
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [page]);
+
+    
     return(
             <>
-            <ButtonPagination className="buttons-top"/>
+            <div className="buttons-top">
+                <button onClick={prev}>Prev</button>
+                <button onClick={next}>Next</button>
+            </div>
             <div className = "images">
                 {user?.map((item) => (
                     <img className = "item"
@@ -35,7 +53,10 @@ const Pagination = () => {
                     />
                 ))}
             </div>
-            <ButtonPagination className="buttons-down" />,
+            <div className="buttons-down">
+                <button onClick={prev}>Prev</button>
+                <button onClick={next}>Next</button>
+            </div>
             </>
     );
 }
