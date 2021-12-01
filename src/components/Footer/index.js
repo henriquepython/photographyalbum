ec70c970cae5d6001761c39db8d63194a2dcad21
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-12-01 11:44:47 -0300

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,9 +6,17 @@ const Footer = () => {
     return(
        
         <footer>
-            <p>info@meusite.com</p>
-            <p>Tel: XXX-XXXX-XXXX</p>
-            <p>Feito por João Henrique. 2021</p>
+            <div class="left box">
+				<div class="content">
+                <h2> Sobre nós</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				</div>
+			</div>
+            <div>
+                <p>info@meusite.com</p>
+                <p>Tel: XXX-XXXX-XXXX</p>
+                <p>Feito por João Henrique. 2021</p>
+            </div>
         </footer>
        
     );
